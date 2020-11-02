158cb1b5d500524ea45b25299ff2be4e8513ce65
Rudyar <arthur.rudy.code@gmail.com>
2020-11-02 19:23:54 +0100

    attendances

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,25 +2,19 @@ class UserMailer < ApplicationMailer
   default from: 'denis.brognard2@yopmail.com'
  
   def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login' 
+    mail(to: @user.email, subject: 'Bienvenue chez nous !')
 
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
-  def event_subscription(user, event)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @user = user 
+  def event_subscription(event_admin, event, user)
+    @event_admin = event_admin 
     @event = event
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @user = user
     @url  = 'http://monsite.fr/login' 
 
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+    mail(to: @event_admin.email, subject: 'Confirmation event') 
+
   end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,10 +1,13 @@
 class Attendance < ApplicationRecord
-  after_create :
+  after_create :subcription_send
+  
   belongs_to :user
   belongs_to :event
 
   def subcription_send
-    UserMailer.event_subscription.deliver_now
+    event = self.event
+    event_admin = event.event_admin
+    UserMailer.event_subscription(event_admin, event, user).deliver_now
   end
 
 end
