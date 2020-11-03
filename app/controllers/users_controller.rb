8ff0f2e43e8619c90f709a6283baa3b15e8e86c2
Rudyar <arthur.rudy.code@gmail.com>
2020-11-03 17:44:36 +0100

    add user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  # before_filter :check_user, :only => [:show]
+
+  def check_user
+     @user = User.find(params[:id])
+     unless current_user.id == @user.id
+        redirect_to root_path
+     end
+  end
+
   def show
-    
+    check_user
+    @user = User.find(params[:id])
+    @events = Event.where(event_admin: @user)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@ require 'faker'
 #   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 50 + rand(150)), email: "denis#{n += 1}@yopmail.com")
 # end
 
-# 3.times do
-#   Event.create(start_date: Faker::Time.between(from: DateTime.now + 1, to: DateTime.now + 60), duration: 25, title: Faker::Lorem.characters(number: 10), 
-#   description: "dhazjdhiuazdiuazuiduazuiduizaudaz", price: 50, location: "Paris", event_admin: User.all.sample)
+3.times do
+  Event.create(start_date: Faker::Time.between(from: DateTime.now + 1, to: DateTime.now + 60), duration: 25, title: Faker::Lorem.characters(number: 10), 
+  description: "dhazjdhiuazdiuazuiduazuiduizaudaz", price: 50, location: "Paris", event_admin: User.all.sample)
 
-# end
+end
 
 # 3.times do
 #   Attendance.create(event_id: Event.all.sample.id, user_id: User.all.sample.id)
